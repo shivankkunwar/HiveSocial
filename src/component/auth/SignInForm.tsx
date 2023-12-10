193bb4cd7a16a9f6b5c9d44ad6d5bc68d31419a6
shivank <shivankkunwar100@gmail.com>
2023-12-11 00:49:44 +0530

    authentication UI completed

---
+++ src/component/auth/SignInForm.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Row, Col, Typography } from "antd";
+import "./Auth"
+const { Title } = Typography;
+function SignInForm() {
+
+
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    type FieldType = {
+        email?: string;
+        password?: string;
+        remember?: string;
+    };
+    return (
+        <div style={{display:"flex", flexDirection:"column"}}>
+        <Title>Log In</Title>
+        <Form
+            name="basic"
+           
+            style={{ width: 300 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item<FieldType>
+                
+                name="email"
+                hasFeedback
+                rules={[
+                    {
+                      type: "email",
+                      message: "The input is not valid E-mail!",
+                    },
+                    {
+                      required: true,
+                      message: "Please input your E-mail!",
+                    },
+                  ]}
+
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="enter email" />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+               
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+                name="remember"
+                valuePropName="checked"
+                
+            >
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item >
+                <Button type="primary" htmlType="submit">
+                   Login
+                </Button>
+            
+            </Form.Item>
+        </Form>
+        </div>
+    )
+}
+
+export default SignInForm

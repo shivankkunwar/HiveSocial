7025302d4badf6319d83c9badadb94fb82d562cd
shivank <shivankkunwar100@gmail.com>
2023-12-15 13:49:48 +0530

    added stylelint css linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import stylelint from 'vite-plugin-stylelint';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), stylelint({
+    configFile: './.stylelintrc',
+    fix: true,
+    formatter: 'string',
+    cache: true,
+  })],
+  
 })

94ede3c309b8d86a72606eb6ad2f11a497cb1b24
shivank <shivankkunwar100@gmail.com>
2023-12-16 01:44:22 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,30 +1,79 @@
-# React + TypeScript + Vite
+# HiveSocial -  Social Media Web App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This project is a small Social Media Web App functional UI  developed using React JS, TypeScript, and Ant Design libraries. The key features of this application include user authentication, post creation/viewing, liking and bookmarking posts, commenting on posts, editing/deleting self-posts and self-comments, and updating profile details.
 
-Currently, two official plugins are available:
+## Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### Authentication Login/ Sign up page
 
-## Expanding the ESLint configuration
+- UI of Both Login and Signup form - Complete form validation.
+-A dummy sign Account is available as a backend mimicking Mock data 
+- `Email: test@test.com`
+- `Password: password`
+- A Modal pops up for success and failure of the Signin and Buttons - Design Principle
+  
+  
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+### Home page
+- Users can see posts they created and others on the timeline.
+- Users can create new posts with textual content.
+- Posts created by the user can be viewed on the 'My Posts' page.
+- Users can like and bookmark any post.
+- Users can edit or delete their own posts.
 
-- Configure the top-level `parserOptions` property like this:
+### Likes and Bookmarks pages
+- user is able to see the posts they have liked. 
+- user is able to see the posts they have bookmarked.
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
+### My posts
+- User is able to see all the posts created by the user
+- Each post is a row in a table, The row have an expandable option to show the comments for that post.
+  
+### Comments
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+- Users can comment on any post.
+- Users can edit or delete their own comments.
+
+### User Profile
+
+- Users can view and edit their profile details on the 'My Profile' page.
+
+## Tech Stack
+
+- React v17+
+- TypeScript
+- Ant Design v5+
+- Ant Design Pro v5+
+- Ant Pro Table!
+- Capacitor.js
+
+## CI/CD
+[Check my github actions](https://github.com/shivankkunwar/HiveSocial/actions/)
+- GitHub Actions were used for CI/CD operations.
+- JS Linting was done using eslint.
+- CSS Linting was done using stylelint.
+
+
+
+## Testing
+
+- Jest  tests have been written to ensure robustness and reliability.
+- 
+-![](https://github.com/shivankkunwar/HiveSocial/assets/66783424/3fc1b89e-ba4d-4bf8-a73a-b9e53011a8be)
+-
+
+## How to run the project
+
+1. Clone the repository: `git clone https://github.com/shivankkunwar/HiveSocial.git`
+2. Navigate to the project directory: `cd HiveSocial`
+3. Install the dependencies: `npm install`
+4. Start the app: `npm run dev`
+
+
+
+## Deployment
+
+The application is deployed on [Hive Social online](https://hive-social.vercel.app/).
+
+
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Post from '../component/post/Post';
 import { usePostContext } from '../../Context/PostContextUtils';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { useState } from 'react';
-
+import "./check.css"
 const ITEMS_PER_PAGE = 10;
 
 type CommentType = {
@@ -17,26 +17,26 @@ type CommentType = {
 };
 
 type PostType = {
-  id:number;
+  id: number;
   name: string;
   photo: string;
   date: string;
   content: string;
   likes: number;
   bookmarks: number;
- 
+
   isOwner: boolean;
   isLiked: boolean;
   isBookmarked: boolean;
-  comments:CommentType[];
+  comments: CommentType[];
 };
 function Home() {
-  const{ posts, setPosts}= usePostContext();
+  const { posts, setPosts } = usePostContext();
   const [hasMoreItems, setHasMoreItems] = useState(true);
   const [items, setItems] = useState<PostType[]>([]);
   console.log(posts)
- 
-  
+
+
   // Define a function to handle the submit event of the post form
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     // Prevent the default behavior of the form
@@ -56,25 +56,25 @@ function Home() {
     const today = new Date();
     const dateString = today.toLocaleDateString('en-US', { year: 'numeric', month: 'numeric', day: 'numeric' });
     const newPost: PostType = {
-      id:Math.random(),
+      id: Math.random(),
       name: 'You',
       photo: `https://robohash.org/hassc1?size=50x50  `,
       date: dateString,
       content: content,
       likes: 0,
       bookmarks: 0,
-      
+
       isOwner: true,
       isLiked: false,
       isBookmarked: false,
       comments: [],
-      
+
     };
-    
+
     // Add the new post to the posts array
-    setPosts((prevState : PostType[]) => {
+    setPosts((prevState: PostType[]) => {
       const newPosts = [newPost, ...prevState];
-      
+
       return newPosts;
     });
 
@@ -95,27 +95,28 @@ function Home() {
 
   // Return the ProCard component for the home page
   return (
-    <ProCard split="vertical" style={{ display: "flex", background: "linear-gradient(#FDFDFD,#F5F5F5, #F5F5F5)" }}>
-      <ProCard title="All posts" colSpan="70%" style={{ display: "flex", background: "linear-gradient(#FDFDFD,#F5F5F5, #F5F5F5)" }}>
-      <InfiniteScroll
+    <ProCard split="vertical" className='pro-card-container'>
+      <ProCard title="All posts" colSpan="70%" className='pro-card-left' style={{ display: "flex", background: "linear-gradient(#FDFDFD,#F5F5F5, #F5F5F5)" }}>
+        <InfiniteScroll
           dataLength={items.length}
           next={fetchMoreData}
           hasMore={hasMoreItems}
           loader={<h4>Loading...</h4>}
-          style={{overflow:"none"}}
+          style={{ overflow: "none" }}
         >
-          {posts.map((post : PostType, index: number) => (
+          {posts.map((post: PostType, index: number) => (
             <Post key={post.id} post={post} id={index} />
           ))}
         </InfiniteScroll>
       </ProCard>
-      <ProCard title="Create Post" colSpan="30%" style={{ background: "linear-gradient(#FDFDFD, #F5F5F5)", }} >
+      <ProCard title="Create Post" colSpan="30%" className='pro-card-right' style={{ background: "linear-gradient(#FDFDFD, #F5F5F5)", }} >
         <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
           <textarea name="content" placeholder="Enter your post content here (max length: 200 characters)" style={{ width: "100%", height: "10vh" }} />
-          <button type="submit" style={{ maxWidth: "70px", backgroundColor: '#1890ff', color: "white", fontSize: "1rem", borderRadius:"1rem" }}>Post</button>
+          <button type="submit" style={{ maxWidth: "70px", backgroundColor: '#1890ff', color: "white", fontSize: "1rem", borderRadius: "1rem" }}>Post</button>
         </form>
       </ProCard>
-    </ProCard>
+
+    </ProCard >
   )
 }
 
